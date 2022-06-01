57f9a34552ab20fa6d595ae4ba891945b519fdca
eunscho <bene@kw.ac.kr>
2022-06-01 21:12:09 +0900

    fff

--- r/analyze_dat.R
+++ r/analyze_dat.R
@@ -20,6 +20,11 @@ analyze_dat <- function(conditions, condition_number, rep_set, rep, data) {
     m_ct <- paste0(m_ct, ';')
     m_ct <- paste0(m_ct, 'T', i, ' ~~ 1 * T', i, ';')
   }
+  for (i in 1:(t - 1)) {
+    for (j in (i + 1):t) {
+      m_ct <- paste0(m_ct, 'T', i, ' ~~ a', i, j, ' * T', j, ';')
+    }
+  }
   #ctcm: ct-correlated methods
   m_ctcm <- m_ct
   for (j in 1:m) {
@@ -30,18 +35,90 @@ analyze_dat <- function(conditions, condition_number, rep_set, rep, data) {
     m_ctcm <- paste0(m_ctcm, ';')
     m_ctcm <- paste0(m_ctcm, 'M', j, ' ~~ 1 * M', j, ';')
   }
+  for (i in 1:(m - 1)) {
+    for (j in (i + 1):m) {
+      m_ctcm <- paste0(m_ctcm, 'M', i, ' ~~ b', i, j, ' * M', j, ';')
+    }
+  }
   for (i in 1:t) {
     for (j in 1:m) {
       m_ctcm <- paste0(m_ctcm, 'T', i, ' ~~ 0 * M', j, ';')
-      m_ctcm <- paste0(m_ctcm, 't', i, 'm', j, ' ~~ c', i, j, ' * t', i, 'm', j, ';')
+      #m_ctcm <- paste0(m_ctcm, 't', i, 'm', j, ' ~~ c', i, j, ' * t', i, 'm', j, ';')
     }
   }
+  # Rindskopf's reparametrization
+  m_ctcmr <- m_ctcm
+  for (i in 1:t) {
+    for (j in 1:m) {
+      m_ctcmr <- paste0(m_ctcmr, 'P', i, j, ' =~ NA * t', i, 'm', j, ';')
+      m_ctcmr <- paste0(m_ctcmr, 'P', i, j, ' ~~ 1 * P', i, j, ';')
+      #m_ctcmr <- paste0(m_ctcmr, 'c', i, j, ' == 0', ';')
+      m_ctcmr <- paste0(m_ctcmr, 't', i, 'm', j, ' ~~ 0 * t', i, 'm', j, ';')
+      for (k in 1:t) {
+        m_ctcmr <- paste0(m_ctcmr, 'P', i, j, ' ~~ 0 * T', k, ';')
+      }
+      for (l in 1:m) {
+        m_ctcmr <- paste0(m_ctcmr, 'P', i, j, ' ~~ 0 * M', l, ';')  
+      }
+    }
+  }
+  for (i in 1:t) {
+    for (j in 1:m) {
+      for (k in i:t) {
+        for (l in 1:m) {
+          if ((i == k) & (l > j) | k > i)  {
+            m_ctcmr <- paste0(m_ctcmr, 'P', i, j, ' ~~ 0 * P', k, l, ';')
+          }
+        }
+      }
+    }
+  }
+  # partial constraining
+  m_ctcmpc <- m_ctcm
+  for (i in 1:t) {
+    for (j in 1:m) {
+      m_ctcmpc <- paste0(m_ctcmpc, 't', i, 'm', j, ' ~~ c', i, j, ' * t', i, 'm', j, ';')
+      m_ctcmpc <- paste0(m_ctcmpc, 'c', i, j, ' > 0;')
+    }
+  }
+  # full constraining
+  m_ctcmfc <- m_ctcmpc
+  for (i in 1:(t - 1)) {
+    for (j in (i + 1):t) {
+      m_ctcmfc <- paste0(m_ctcmfc, 'a', i, j, ' > -1;')
+      m_ctcmfc <- paste0(m_ctcmfc, 'a', i, j, ' < 1;')
+    }
+  }
+  for (i in 1:(m - 1)) {
+    for (j in (i + 1):m) {
+      m_ctcmfc <- paste0(m_ctcmfc, 'b', i, j, ' > -1;')
+      m_ctcmfc <- paste0(m_ctcmfc, 'b', i, j, ' < 1;')
+    }
+  }
+  
+
   # ctcu: ct-correlated uniqueness
   m_ctcu <- m_ct
   for (j in 1:m) {
     for (i in 1:(t - 1)) {
       for (k in (i + 1):t)
         m_ctcu <- paste0(m_ctcu, 't', i, 'm', j, ' ~~ t', k, 'm', j, '; ')
+    }
+  }
+  #ctmin: ct-correlated (m-1)methods
+  m_ctmin <- m_ct
+  for (j in 2:m) { # not 1, because m-1 
+    m_ctmin <- paste0(m_ctmin, 'M', j, ' =~ NA * t1m', j)
+    for (i in 2:t) {
+      m_ctmin <- paste0(m_ctmin, '+ t', i, 'm', j)
+    }
+    m_ctmin <- paste0(m_ctmin, ';')
+    m_ctmin <- paste0(m_ctmin, 'M', j, ' ~~ 1 * M', j, ';')
+  }
+  for (i in 1:t) {
+    for (j in 2:m) {
+      m_ctmin <- paste0(m_ctmin, 'T', i, ' ~~ 0 * M', j, ';')
+      #m_ctmin <- paste0(m_ctmin, 't', i, 'm', j, ' ~~ c', i, j, ' * t', i, 'm', j, ';')
     }
   }
   
--- r/my_sample.R
+++ r/my_sample.R
@@ -5,23 +5,23 @@ my_sample <- function(base, rep) {
   pattern <- sample(c(1, 2, 3, 4, 5, 6), qt, replace = TRUE)
   for (i in 1:length(pattern)) {
     if (pattern[i] == 1) {
-      out[3 * (i - 1) + 1: 3 * (i - 1) + 3] <- c(0, -.1, .1)
+      out[(3 * (i - 1) + 1): (3 * (i - 1) + 3)] <- c(0, -.1, .1)
     } else if (pattern[i] == 2) {
-      out[3 * (i - 1) + 1: 3 * (i - 1) + 3] <- c(0, .1, -.1)
+      out[(3 * (i - 1) + 1): (3 * (i - 1) + 3)] <- c(0, .1, -.1)
     } else if (pattern[i] == 3) {
-      out[3 * (i - 1) + 1: 3 * (i - 1) + 3] <- c(.1, -.1, 0)
+      out[(3 * (i - 1) + 1): (3 * (i - 1) + 3)] <- c(.1, -.1, 0)
     } else if (pattern[i] == 4) {
-      out[3 * (i - 1) + 1: 3 * (i - 1) + 3] <- c(.1, 0, -.1)
+      out[(3 * (i - 1) + 1): (3 * (i - 1) + 3)] <- c(.1, 0, -.1)
     } else if (pattern[i] == 5) {
-      out[3 * (i - 1) + 1: 3 * (i - 1) + 3] <- c(-.1, 0, .1)
+      out[(3 * (i - 1) + 1): (3 * (i - 1) + 3)] <- c(-.1, 0, .1)
     } else {
-      out[3 * (i - 1) + 1: 3 * (i - 1) + 3] <- c(-.1, .1, 0)
+      out[(3 * (i - 1) + 1): (3 * (i - 1) + 3)] <- c(-.1, .1, 0)
     }
   }
   if (rmd == 1) {
     out[rep] <- 0
   } else if (rmd == 2) {
-    out[rep - 1: rep ] <- c(.1, -.1)
+    out[(rep - 1): rep ] <- c(.1, -.1)
   }
   out <- base + out
   return(out)
